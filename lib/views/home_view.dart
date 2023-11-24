886dfcc0b66027e25ea6ac0642932c179796aeca
Riyam224 <cs50riyam211#gmail.com>
2023-11-25 00:36:31 +0300

    make card in blue and add some properties

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'News',
-              style: TextStyle(color: Colors.black),
-            ),
-            const Text(
-              ' Clound',
-              style: TextStyle(color: Color.fromARGB(255, 174, 137, 25)),
-            ),
-          ],
+        appBar: AppBar(
+          elevation: 0,
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'News',
+                style: TextStyle(color: Colors.black),
+              ),
+              const Text(
+                ' Clound',
+                style: TextStyle(color: Color.fromARGB(255, 174, 137, 25)),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.transparent,
         ),
-        backgroundColor: Colors.transparent,
-      ),
-      body: Column(),
-    );
+        body: Container(
+          height: 85,
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: const Center(child: Text('hello')),
+        ));
   }
 }
