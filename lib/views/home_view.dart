68e1881ca76a304a710325048ba96a1f5325b22c
Riyam224 <cs50riyam211#gmail.com>
2023-11-25 00:19:28 +0300

    make app bar

---
+++ lib/views/home_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            const Text(
+              ' Clound',
+              style: TextStyle(color: Color.fromARGB(255, 174, 137, 25)),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(),
+    );
+  }
+}
